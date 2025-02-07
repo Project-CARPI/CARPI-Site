84b9c4d5d24b07bae5adaefd8301ecf603c8b26e
Gavin Liu <85533492+liug88@users.noreply.github.com>
2025-02-07 17:08:51 -0500

    Initial Department Filter Code
    Starter code and figma image for the Department Filters, which will filter courses by departments when clicked on.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Catalog from "./pages/Catalog";
 import Planner from "./pages/Planner";
+import DepartmentFilters from "./components/Department-Filters";
 
 function App() {
   return (
-    <>
-      <div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Catalog />}></Route>
-            <Route path="/planner" element={<Planner />}></Route>
-          </Routes>
-        </Router>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Catalog />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/filters" element={<DepartmentFilters />} />
+      </Routes>
+    </Router>
   );
 }
 
