de8741fcdf41ea00e93318ea0ea677846e8d3c72
Justin Zhang <justinzwork23@gmail.com>
2025-02-08 02:39:44 -0500

    completed animation
    idk whats better animation or none

--- src/components/Course/Course.tsx
+++ src/components/Course/Course.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Tag from "./Tag";
 import AddButton from "./AddButton";
+import { motion } from "framer-motion";
 
 interface CourseType {
   id: number;
@@ -18,28 +19,48 @@ interface CourseProps {
 }
 
 const Course: React.FC<CourseProps> = ({ course }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => {
+    setIsOpen((open) => !open);
+  };
+
   return (
     <>
       <div
-        className={`bg-[#F5CECE] border-2 border-black w-11/12 rounded-xl p-4 m-auto mt-2 font-['Helvetica'] flex items-center justify-between`}
+        className={`bg-[#F5CECE] border-2 border-black w-11/12 rounded-xl p-4 m-auto mt-2 
+          font-['Helvetica']`}
+        onClick={toggleOpen}
       >
-        <div className={`w-11/12`}>
-          <p className={`text-xl`}>
-            {course.department}
-            {course.code} {course.name}
-          </p>
-          <div className={`flex flex-wrap mt-1`}>
-            <Tag name={course.department} color={"4D5E87"} />
-            {course.attributesList.map((attr) => {
-              return <Tag name={attr} color={"4D5E87"} />;
-            })}
-            {course.semestersOffered.map((semester) => {
-              return <Tag name={semester} color={"4D5E87"} />;
-            })}
+        <div className={`flex items-center justify-between`}>
+          <div className={`w-11/12`}>
+            <p className={`text-xl`}>
+              {course.department}
+              {course.code} {course.name}
+            </p>
+            <div className={`flex flex-wrap mt-1`}>
+              <Tag name={course.department} color={"4D5E87"} />
+              {course.attributesList.map((attr) => {
+                return <Tag name={attr} color={"4D5E87"} />;
+              })}
+              {course.semestersOffered.map((semester) => {
+                return <Tag name={semester} color={"4D5E87"} />;
+              })}
+            </div>
+          </div>
+          <div className={``}>
+            <AddButton />
           </div>
         </div>
-        <div className={``}>
-          <AddButton />
+        <div className={`${isOpen ? "" : "hidden"} mt-2`}>
+          <motion.p
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "auto", opacity: 1 }}
+            className={`text-sm`}
+            transition={{ duration: 0.05 }}
+            key={isOpen}
+          >
+            {course.description}
+          </motion.p>
         </div>
       </div>
     </>
