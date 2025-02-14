d9d7579e146049721ca1645317d373ba7e96b3cf
Justin Zhang <justinzwork23@gmail.com>
2025-02-14 17:38:53 -0500

    added toggle, need to fix logic and clickability

--- src/components/PlannerComponents/PlannerCourse.tsx
+++ src/components/PlannerComponents/PlannerCourse.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MdDragIndicator, MdOutlineMoreHoriz } from "react-icons/md";
 import { CourseType } from "../../types/interfaces/Course.interface";
 
@@ -7,10 +7,34 @@ interface PlannerCourseProps {
 }
 
 const PlannerCourse: React.FC<PlannerCourseProps> = ({ course }) => {
+  const [openPopup, setOpenPopup] = useState<boolean>(true);
+  const togglePopup = () => {
+    setOpenPopup((open) => !open);
+  };
+
+  const componentRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      componentRef.current &&
+      event.target instanceof Node &&
+      !componentRef.current.contains(event.target)
+    ) {
+      setOpenPopup(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <>
       <div
-        className={`bg-[#283044] w-3/4 h-18 rounded-lg text-[#F5CECE] flex items-center`}
+        className={`bg-[#283044] w-3/4 h-18 rounded-lg text-[#F5CECE] flex items-center relative`}
       >
         <div className={`flex justify-between w-11/12 m-auto text-2xl`}>
           <div className={`flex items-center`}>
@@ -29,7 +53,21 @@ const PlannerCourse: React.FC<PlannerCourseProps> = ({ course }) => {
             >
               <p>{course.credits}</p>
             </div>
-            <MdOutlineMoreHoriz />
+            <MdOutlineMoreHoriz onClick={togglePopup} />
+          </div>
+        </div>
+        <div
+          className={`${openPopup ? "hidden" : ""} absolute h-fit w-fit bg-[#F5CECE] rounded-xl border-1 border-slate-500 text-[#283044] text-xs -right-4 -bottom-20 *:*:px-1 p-1 *:*:py-1 *:pr-6`}
+          ref={componentRef}
+        >
+          <div className={`h-1/2 relative flex flex-col items-start pb-1 `}>
+            <button>Duplicate</button>
+            <button>Move to next sem</button>
+          </div>
+          <hr />
+          <div className={`h-1/2 relative flex flex-col items-start pt-1`}>
+            <button>Back to toolbox</button>
+            <button>Delete</button>
           </div>
         </div>
       </div>
