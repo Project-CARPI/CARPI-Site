98c28e31a14c9ab108d884b6f8669ab4d654a8ff
CharlieC133 <69218910+CharlieC133@users.noreply.github.com>
2025-02-25 16:04:39 -0500

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import fetchAPI from "./components/departmentsFetch/fetchAPI.tsx";
+//import App from "./App.tsx";
+import MultiSelector from "./components/MultiSelect/Multiselect.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+axios
+  .get("http://carpi.cs.rpi.edu:8000/api/v1/course/filter/values/departments")
+  .catch((error) => console.error("Error fetching data:", error));
+
+<MultiSelector
+items={["type", "scale", "participants", "range", "tags"]}
+selectedItems={selectedItems}
+setSelectedItems={handleSelectorChange}
+/>
